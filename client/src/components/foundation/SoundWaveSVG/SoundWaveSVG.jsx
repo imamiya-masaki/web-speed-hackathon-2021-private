7fc4702bdd3353f1b2653ed0bce42d3db0327500
imamiya-masaki <imamiyagit@gmail.com>
2021-12-17 02:50:57 +0900

    使っていない関数を削除

--- client/src/components/foundation/SoundWaveSVG/SoundWaveSVG.jsx
+++ client/src/components/foundation/SoundWaveSVG/SoundWaveSVG.jsx
@@ -1,34 +1,8 @@
-import _ from 'lodash';
 import React from 'react';
 /**
  * @param {ArrayBuffer} data
  * @returns {Promise<{ max: number, peaks: number[] }}
  */
-async function calculate(data) {
-  const audioCtx = new AudioContext();
-
-  // 音声をデコードする
-  /** @type {AudioBuffer} */
-  const buffer = await new Promise((resolve, reject) => {
-    audioCtx.decodeAudioData(data.slice(0), resolve, reject);
-  });
-  console.log('buffer', buffer, buffer.getChannelData(0), buffer.getChannelData(1), data);
-  // 左の音声データの絶対値を取る
-  const leftData = _.map(buffer.getChannelData(0), Math.abs);
-  // 右の音声データの絶対値を取る
-  const rightData = _.map(buffer.getChannelData(1), Math.abs);
-
-  // 左右の音声データの平均を取る
-  const normalized = _.map(_.zip(leftData, rightData), _.mean);
-  // 100 個の chunk に分ける
-  const chunks = _.chunk(normalized, Math.ceil(normalized.length / 100));
-  // chunk ごとに平均を取る
-  const peaks = _.map(chunks, _.mean);
-  // chunk の平均の中から最大値を取る
-  const max = _.max(peaks);
-
-  return { max, peaks };
-}
 
 /**
  * @typedef {object} Props
