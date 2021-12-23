e9ee54692246b973457e2603d2eef1a011bd6a2a
imamiya-masaki <imamiyagit@gmail.com>
2021-12-24 02:47:24 +0900

    asyncを戻す

--- client/src/components/foundation/PausableMovie/PausableMovie.jsx
+++ client/src/components/foundation/PausableMovie/PausableMovie.jsx
@@ -72,7 +72,7 @@ const PausableMovie = ({ src }) => {
   return (
     <AspectRatioBox aspectHeight={1} aspectWidth={1}>
       <button className="group relative block w-full h-full" onClick={handleClick} type="button">
-        <canvas ref={canvasCallbackRef} className="w-full" width="574" height="574" async/>
+        <canvas ref={canvasCallbackRef} className="w-full" width="574" height="574"/>
         <div
           className={classNames(
             'absolute left-1/2 top-1/2 flex items-center justify-center w-16 h-16 text-white text-3xl bg-black bg-opacity-50 rounded-full transform -translate-x-1/2 -translate-y-1/2',
